44707e2329122a3686325137bcc24e48cddb9940
suraj-sahani <suraj.sahani.961@gmail.com>
2025-11-15 00:22:07 +0530

    added 4 sum better approach

--- src/arrays/4_sum.ts
+++ src/arrays/4_sum.ts
@@ -53,5 +53,35 @@ function sum4Brute(nums: number[], target: number) {
   return ans
 }
 
-const res = sum4Brute([1, 0, -1, 0, -2, 2], 0)
+// Better Approach
+// Remove the 4th loop and use the other 3 elements to find the 4th element
+// We store the all the elements betwwen the jth and kth iteration in a set
+// And check if the forth element is in this set.
+// TC - O(n^3 * log number_of_quads) : log m for the searching elements in the hashSet
+// TC - O(2 log number_of_quads) : one for returning answer, another for storing unique quads + O(n) : the  temp hashSet
+// used to find the forth element
+function sum4Better(nums: number[], target: number) {
+  const n = nums.length, ans: number[][] = [], st = new Set<string>()
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      const hashSet = new Set<number>()
+      for (let k = j + 1; k < n; k++) {
+        const toFind = target - (nums[i]! + nums[j]! + nums[k]!)
+
+        if (hashSet.has(toFind)) {
+          const quad = [nums[i]!, nums[j]!, nums[k]!, toFind].sort((a, b) => a - b)
+          st.add(JSON.stringify(quad))
+        }
+
+        hashSet.add(nums[k]!)
+      }
+    }
+  }
+
+  st.forEach(val => ans.push(JSON.parse(val)))
+  return ans
+}
+
+const res = sum4Better([1, 0, -1, 0, -2, 2], 0)
 console.log(res)
