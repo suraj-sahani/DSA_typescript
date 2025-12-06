8e1b3fd426a27340c6505f27c599246f9399469d
suraj-sahani <suraj.sahani.961@gmail.com>
2025-12-06 11:03:18 +0530

    Updated to account for overflow

--- src/binary-search/bs.ts
+++ src/binary-search/bs.ts
@@ -5,6 +5,18 @@ function binary_search_iterative(nums: number[], target: number) {
   let low = 0, high = n - 1, mid
 
   while (low <= high) {
+    // We calculate mid with the approach given because
+    // programming languages have an interger limit that can be stored
+    // Thus, there might be a case that the upper limit is that INT_MAX
+    // and if we are searching for the last element,
+    // mid = INT_MAX + INT_MAX / 2, this will cause an overflow
+    // To prevent this, we add floow of (high - low)/ 2 which will prevent
+    // the overflow. How ?
+    // E.g we are searching for the last element => low = high = INT_MAX
+    // This low + Math.floor( (high - low) / 2)
+    // mid = INT_MAX + Math.floor( (INT_MAX - INT_MAX) / 2)
+    // mid = INT_MAX + Math.floor(( 0 / 2)) => INT_MAX
+    // Thereby preventing the overflow
     mid = low + Math.floor((high - low) / 2)
 
     if (nums[mid]! === target) return mid
