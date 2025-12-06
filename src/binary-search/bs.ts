ea70163a61859a9c5d712af2becabae1cb0fdaf8
suraj-sahani <suraj.sahani.961@gmail.com>
2025-12-06 10:55:30 +0530

    added recursive approach

--- src/binary-search/bs.ts
+++ src/binary-search/bs.ts
@@ -1,9 +1,11 @@
+// Iterative Aproach
+// TC - O(log n)
 function binary_search_iterative(nums: number[], target: number) {
   const n = nums.length
   let low = 0, high = n - 1, mid
 
   while (low <= high) {
-    mid = (low + high) / 2
+    mid = low + Math.floor((high - low) / 2)
 
     if (nums[mid]! === target) return mid
     else if (nums[mid]! < target) low = mid + 1
@@ -13,5 +15,16 @@ function binary_search_iterative(nums: number[], target: number) {
   return -1
 }
 
-const res = binary_search_iterative([1, 2, 5, 8, 12, 15, 21], 100)
+// Recursive Approach
+// TC - O(log n)
+function binary_search_recursive(nums: number[], low: number, high: number, target: number) {
+  if (low > high) return -1
+
+  let mid = low + Math.floor((high - low) / 2)
+  if (nums[mid] === target) return mid
+  else if (nums[mid]! < target) return binary_search_recursive(nums, mid + 1, high, target)
+  else return binary_search_recursive(nums, low, mid - 1, target)
+}
+
+const res = binary_search_recursive([1, 2, 5, 8, 12, 15, 21], 0, 6, 8)
 console.log(res)
