91744629490f81a384066a2a44929e3bcf0b137f
suraj-sahani <suraj.sahani.961@gmail.com>
2025-12-25 00:03:09 +0530

    added appraoch without lower and upper bound concepts

--- src/binary-search/first_and_last_occurence.ts
+++ src/binary-search/first_and_last_occurence.ts
@@ -31,9 +31,9 @@
 // Brute Force Approach
 // Iterate through the array
 // If target is found and there was no value of
-// the first occurenece, then that is the first occurenece and maybe the last,
-// thus we update both first and last occurenece values,
-// continue iterating and update last occurenece is the target is found
+// the first occurrence, then that is the first occurenece and maybe the last,
+// thus we update both first and last occurrence values,
+// continue iterating and update last occurrence is the target is found
 // TC - O(n)
 // SC - O(1)
 function brute(nums: number[], x: number) {
@@ -43,7 +43,7 @@ function brute(nums: number[], x: number) {
   for (let i = 0; i < n; i++) {
     // Check if the current number is the target
     if (nums[i] === x) {
-      // If it is set the value of first occurenece only if
+      // If it is set the value of first occurrence only if
       // it was not set before
       if (fOcc === -1) fOcc = i
       // Update last everytime
@@ -55,8 +55,8 @@ function brute(nums: number[], x: number) {
 }
 
 // We will use the same principal of lower and upper bound
-// Lower Bound : Smallest index where nums[i] >= x  => First occurenece
-// Upper Bound : Smallest index where nums[i] > x => Last occurenece
+// Lower Bound : Smallest index where nums[i] >= x  => First occurrence
+// Upper Bound : Smallest index where nums[i] > x => Last occurrence
 // TC - Lower Bound : O(log n) + Upper Bound : O(log n) => O(2 log n) ~ O(log n)
 // SC - O(1)
 function optimal(nums: number[], x: number) {
@@ -65,7 +65,7 @@ function optimal(nums: number[], x: number) {
   let low = 0, high = n - 1, fOcc = -1, lOcc = -1, mid
 
 
-  // Find the first occurenece using lower bound
+  // Find the first occurrence using lower bound
   while (low <= high) {
     mid = low + Math.floor((high - low) / 2)
 
@@ -77,7 +77,7 @@ function optimal(nums: number[], x: number) {
       low = mid + 1
   }
 
-  // Find the last occurenece using upper bound
+  // Find the last occurrence using upper bound
   low = 0, high = n - 1
 
   while (low <= high) {
@@ -109,12 +109,72 @@ function optimal(nums: number[], x: number) {
   // and thus, return [-1,-1]
   if (fOcc === n || nums[fOcc] !== x) return [-1, -1]
 
-  // We are subtracting -1 from the last occurenece,
+  // We are subtracting -1 from the last occurrence,
   // becuase upper bound gives the the index where the number is greater 
-  // than the target and since we require the index of the last occurenece
+  // than the target and since we require the index of the last occurrence
   // we subtract it from the index where the element is greater than the target
   return [fOcc, lOcc - 1]
 }
 
-const res = optimal([2, 2, 4, 4, 8, 8, 8, 8, 11, 13], 10)
+// We dont really need to rely on the upper bound
+// and lower bound concepts
+// TC - O(2 log n)
+// SC - O(1)
+function optimalV2(nums: number[], x: number) {
+  const n = nums.length
+
+  let low = 0, high = n - 1, first = -1, last = -1, mid
+
+  // Finding first occurence
+  while (low <= high) {
+    mid = low + Math.floor((high - low) / 2)
+
+    // Check if current number is equal ot x
+    // if it is,update first and since we want the first index,
+    // we limit our search space to the left side
+    if (nums[mid]! === x) {
+      first = mid
+      high = mid - 1
+    }
+    // If current value is less than x,
+    // then x can be on the right half
+    else if (nums[mid]! < x) low = mid + 1
+
+    // If current number is greater than the target,
+    // we limit the search space to the left side
+    else
+      high = mid - 1
+  }
+
+  low = 0, high = n - 1
+
+  // If there is no first occurence
+  // then there also will be no last occurrence,
+  // thus we return [-1,-1]
+  if (first === -1) return [-1, -1]
+
+  // Finding last occurence
+  while (low <= high) {
+    mid = low + Math.floor((high - low) / 2)
+
+    // If the current value is equal to x,
+    // if can be out last occurence and since we need the last 
+    // occurence, we update our search space to the right side
+    if (nums[mid] === x) {
+      last = mid,
+        low = mid + 1
+    }
+    // If the current value is smaller than x,
+    // we update the search space to the right
+    else if (nums[mid]! < x) low = mid + 1
+
+    // If current value is greater than x,
+    // we need to limit the search space to the left side
+    else high = mid - 1
+  }
+
+  return [first, last]
+}
+
+const res = optimalV2([2, 2, 4, 4, 8, 8, 8, 8, 11, 13], 10)
 console.log(res)
