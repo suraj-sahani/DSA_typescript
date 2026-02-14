ab42ef5fa74f398e45f1b611da037c52447e06f7
suraj-sahani <suraj.sahani.961@gmail.com>
2026-02-14 23:22:56 +0530

    feat: Add seach in sorted array 2 optimal approach

--- src/binary-search/search_in_rotate_sorted_array_2.ts
+++ src/binary-search/search_in_rotate_sorted_array_2.ts
@@ -14,6 +14,9 @@
 // Result: False
 // Explanation: The element 10 is not present in the array. So, the answer is False.
 
+// Brute Force approach
+// Iterate through the array and search for the target
+// If we find the target, return true else false
 function brute(nums: number[], target: number) {
   const n = nums.length
 
@@ -24,5 +27,65 @@ function brute(nums: number[], target: number) {
   return false
 }
 
-const res = brute([7, 8, 1, 2, 3, 3, 3, 4, 5, 6], -1)
+// Optimal approach
+// If the previous problem, the first step was to identify which half is sorted
+// Once we were able to find the sorted half, we would discard the unsorted half 
+// and continue binary-search on the sorted half.
+// That approach will not work here.
+// Example: nums: [6, 7, 1, 2, 3, 4, 4, 5], target : 4
+// We can clearly see that [2, 3, 4, 4, 5] is the sorted half
+// But, for  nums: [3, 1, 2, 3, 3, 3, 3], 
+// low = 0 => nums[0] = 3
+// mid = 3 => nums[3] = 3
+// high = 6 => nums[6] = 3
+// We can clearly see, the low, mid and high are all equal,
+// we cannot identify the sorted half programatically
+// Fix:
+// The problem is that arr[low] === arr[mid] === arr[high]
+// thus, whenever we face this conditions, we increment low and decrement high 
+// and continue the same process of finding the sorted half
+function optimal(nums: number[], target: number) {
+  const n = nums.length
+
+  let low = 0, mid, high = n - 1
+
+  while (low <= high) {
+    mid = low + Math.floor((high - low) / 2)
+
+    if (nums[low] === nums[mid] && nums[mid] === nums[high]) {
+      low++
+      high--
+      continue
+    }
+
+    if (nums[mid] === target) return true
+
+    // Check if left half is sorted
+    if (nums[low]! <= nums[mid]!) {
+      // Check if the sorted half can contain the target,
+      // if it can, update the search space
+      if (nums[low]! <= target && target <= nums[mid]!) {
+        high = mid - 1
+      } else {
+        low = mid + 1
+      }
+    }
+
+    // if the right half is sorted
+    else {
+      // Check if the target can lie in the right half
+      // If it can update the search space
+      if (nums[mid + 1]! <= target && target <= nums[high]!)
+        low = mid + 1
+      else {
+        high = mid - 1
+      }
+    }
+
+  }
+
+  return false
+}
+
+const res = optimal([3, 1, 2, 3, 3, 3, 3], -1)
 console.log(res)
