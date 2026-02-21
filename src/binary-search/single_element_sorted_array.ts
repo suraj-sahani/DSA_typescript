aba7dd551258fc67bdc4ea55335000ba6f525b04
suraj-sahani <suraj.sahani.961@gmail.com>
2026-02-21 21:31:01 +0530

    feat: Add single element in sorted array - optimal

--- src/binary-search/single_element_sorted_array.ts
+++ src/binary-search/single_element_sorted_array.ts
@@ -57,20 +57,23 @@ function brute(nums: number[]) {
 function brute2(nums: number[]) {
   const n = nums.length
 
+  // Edge case for single element array
+  if (n === 0) return nums[0]!
+
   for (let i = 0; i < n; i++) {
     // Edge case for first element
     // the element before it is not present, thus we check on to the right
     if (i === 0) {
-      if (nums[i] !== nums[i + 1]) return nums[i]
+      if (nums[i] !== nums[i + 1]) return nums[i]!
     }
     // Edge for for the last element,
     // the element to the right is not available, thus we check with the element before it
     else if (i === n - 1) {
-      if (nums[i] !== nums[i - 1]) return nums[i]
+      if (nums[i] !== nums[i - 1]) return nums[i]!
     }
     // Check for both left and right
     else {
-      if (nums[i] !== nums[i - 1] && nums[i] !== nums[i + 1]) return nums[i]
+      if (nums[i] !== nums[i - 1] && nums[i] !== nums[i + 1]) return nums[i]!
     }
   }
 
@@ -97,10 +100,78 @@ function better(nums: number[]) {
   return nonRepeated
 }
 
-
+// Binary Search
+// Observation:
+// arr : [1,1,2,2,3,3,4,5,5,6,6]
+// If we observe, we can see that on the left half of the single element,
+// The first occurrence of the element is on the even index and the next occurrence
+// is on the odd index
+// Similarly, if we look to the right half of the single element,
+// The first occurrence is an odd index and the next occurrence is an even index.
+// Conclusions:
+// So, in Binary Search, if we are in the (even, odd) pattern,
+// then the single element is on the right half
+// And if we are in the (odd, even), the single element is 
+// on the left half. 
+// We can see this surely.
+// There is also edge cases while doing Binary Search,
+// where we will have to check the left
+// and the right to find the duplicates and for this, we will have to handle
+// edge case where either the left or the right is not present.
+// To prevent this, we will take low as 1 instead of 0 and high as n - 2 instead of n - 1
+// and handle those previosuly mentioned case before we implement Binary Search
+// TC - (log n)
+// SC - O(1)
 function optimal(nums: number[]) {
+  const n = nums.length
+
+  // Handling edge case before starting Binary Search
+  if (n === 1) return nums[0]!
+  if (nums[0] !== nums[1]) return nums[0]!
+  if (nums[n - 1] !== nums[n - 1]) return nums[n - 1]!
+
+  let low = 1, high = n - 2, mid;
+  while (low <= high) {
+    mid = low + Math.floor((high - low) / 2)
+
+    // Check if mid is single element
+    if (nums[mid] !== nums[mid - 1] && nums[mid] !== nums[mid + 1]) return nums[mid]
+
+    // Figure out which half will cannot contain the single element
+    // and eliminate it.
+    // (even, odd) => eliminate the left half as element is on the right
+    // (odd, even) => element the right half as element is on the left
+    // How?
+    // Case 1: Standing at an odd index
+    // If the current index is odd and the element before it is the same as the 
+    // current index, that mean we are on the left half
+    // eliminate the left half
+    // else
+    // If the current index is odd and the element before is not the same, then
+    // we are on the right half, thus, element the right half
+    // Case 2: Standing at an even index
+    // If the current index is even and the next element is the same as the current 
+    // index, we are on the left half eliminate it.
+    // else
+    // If the current index is even and the element before it is the same as the current,
+    // we are on the right half, eliminate it.
+
+    // If we are standing at an odd index we check with the right
+    // If we are at an even index, we check with the left
+    // We are on the left half and the single element is on the right
+    // So we eliminate the left half
+    if ((mid % 2 !== 0 && nums[mid - 1] === nums[mid]) || (mid % 2 === 0 && nums[mid] === nums[mid + 1])) {
+      low = mid + 1
+    }
+    // If the above case are not satisfying, that means
+    // We are on the right half and the single element is on the left
+    // So we eliminate the right half
+    else {
+      high = mid - 1
+    }
+  }
 
 }
 
-const res = brute2([1, 1, 2, 3, 3, 4, 4, 8, 8])
+const res = optimal([1, 1, 2, 2, 3, 3, 4, 5, 5, 8, 8])
 console.log(res)
