3c20fbdbd0cdbc1b2f5b3ace224c2c082e57a202
suraj-sahani <suraj.sahani.961@gmail.com>
2026-03-16 18:00:29 +0530

    feat: Add find nth root of number - optimal

--- src/binary-search-on-answers/find_nth_root_of_number.ts
+++ src/binary-search-on-answers/find_nth_root_of_number.ts
@@ -29,5 +29,37 @@ function brute(n: number, m: number) {
   return ans;
 }
 
-const res = brute(4, 81)
+
+// Optimal Appraoch
+// Use binary search on answers
+// TC - O(log m * log n)
+// SC - O(1)
+function optimal(n: number, m: number) {
+  let low = 1, high = m - 1, mid, ans = -1;
+
+  while (low <= high) {
+    mid = low + Math.floor((high - low) / 2)
+
+    // Check if nth power of mid is equal to m
+    // if it is return it.
+    if (mid ** n === m) {
+      ans = mid
+      break;
+    }
+
+    // If nth power of mid exceeds m,
+    // there can be an nth power to the left 
+    // Eliminate the right search space
+    else if (mid ** n > m) high = mid - 1
+
+    // If the nth power of mid is smaller,
+    // then there can be an answer to the right,
+    // Eliminate the left search space
+    else low = mid + 1
+  }
+
+  return ans
+}
+
+const res = optimal(4, 80)
 console.log(res)
